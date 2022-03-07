4015f8335a2c557e61c60d3796e5b24a467ade09
sieuvong-tiki <sieu.vong@tiki.vn>
2022-03-07 18:15:20 +0700

    Update astra config

--- packages/extension/src/config.ts
+++ packages/extension/src/config.ts
@@ -2,6 +2,10 @@ import { Bech32Address } from "@keplr-wallet/cosmos";
 import { ChainInfo } from "@keplr-wallet/types";
 
 import {
+  ASTRA_REST_CONFIG,
+  ASTRA_REST_ENDPOINT,
+  ASTRA_RPC_CONFIG,
+  ASTRA_RPC_ENDPOINT,
   PRIVILEGED_ORIGINS,
   COSMOS_REST_CONFIG,
   COSMOS_REST_ENDPOINT,
@@ -98,6 +102,51 @@ import {
 } from "./config.var";
 
 export const EmbedChainInfos: ChainInfo[] = [
+  {
+    rpc: ASTRA_RPC_ENDPOINT,
+    rpcConfig: ASTRA_RPC_CONFIG,
+    rest: ASTRA_REST_ENDPOINT,
+    restConfig: ASTRA_REST_CONFIG,
+    chainId: "dev-sentry-1",
+    chainName: "Astra",
+    stakeCurrency: {
+      coinDenom: "STAKE",
+      coinMinimalDenom: "stake",
+      coinDecimals: 6,
+      coinGeckoId: "stake",
+    },
+    walletUrl:
+      process.env.NODE_ENV === "production"
+        ? "https://wallet.keplr.app/#/cosmoshub/stake"
+        : "http://localhost:8080/#/cosmoshub/stake",
+    walletUrlForStaking:
+      process.env.NODE_ENV === "production"
+        ? "https://wallet.keplr.app/#/cosmoshub/stake"
+        : "http://localhost:8080/#/cosmoshub/stake",
+    bip44: {
+      coinType: 118,
+    },
+    bech32Config: Bech32Address.defaultBech32Config("cosmos"),
+    currencies: [
+      {
+        coinDenom: "STAKE",
+        coinMinimalDenom: "stake",
+        coinDecimals: 6,
+        coinGeckoId: "stake",
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: "STAKE",
+        coinMinimalDenom: "stake",
+        coinDecimals: 6,
+        coinGeckoId: "stake",
+      },
+    ],
+    coinType: 118,
+    features: ["stargate", "ibc-transfer", "no-legacy-stdTx", "ibc-go"],
+    gasPriceStep: { low: 0.1, average: 0.5, high: 0.8 },
+  },
   {
     rpc: COSMOS_RPC_ENDPOINT,
     rpcConfig: COSMOS_RPC_CONFIG,
